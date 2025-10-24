1430f035898056e3e869f59fd33e54be9b914622
vyminh <22521710@gm.uit.edu.vn>
2025-10-24 14:12:32 +0700

    update login ui - fix button login

--- app/src/main/java/com/dev/thecodecup/MainActivity.kt
+++ app/src/main/java/com/dev/thecodecup/MainActivity.kt
@@ -4,26 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.compose.rememberNavController
 import com.dev.thecodecup.model.network.NetworkTest
 import com.dev.thecodecup.ui.NavGraph
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        
+
         // 🧪 Test API Connection - Check Logcat with filter: "NetworkTest"
         NetworkTest.testApiConnection()
-        
+
         setContent {
             // 🎨 TEMPORARY: Using ApiTestScreen to see products in UI
             // Comment this and uncomment NavGraph below to restore normal app
 //            ApiTestScreen()
-            
+
             // Normal app navigation (temporarily commented)
              val navController = rememberNavController()
              NavGraph(navController)
         }
     }
 }
+
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        // Gọi layout XML
+//        setContentView(R.layout.login)
+//
+//        // Test API Connection - Check Logcat with filter: "NetworkTest"
+//        NetworkTest.testApiConnection()
+//    }
+//}
